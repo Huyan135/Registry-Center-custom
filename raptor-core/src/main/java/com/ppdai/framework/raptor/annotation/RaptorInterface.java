43e37db82ff02138ccde9cb69a5c6c1d72715420
Huyan135 <31300464+Huyan135@users.noreply.github.com>
2018-10-13 18:21:57 +0800

    Add files via upload

---
+++ raptor-core/src/main/java/com/ppdai/framework/raptor/annotation/RaptorInterface.java
@@ -0,0 +1,22 @@
+package com.ppdai.framework.raptor.annotation;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface RaptorInterface {
+
+    String appId() default "";
+
+    String appName() default "";
+
+    String version() default "";
+
+    String protoFile() default "";
+
+    String library() default "";
+
+    String crc32() default "";
+
+}
---
+++ raptor-core/src/main/java/com/ppdai/framework/raptor/common/RaptorConstants.java
@@ -0,0 +1,17 @@
+package com.ppdai.framework.raptor.common;
+
+public class RaptorConstants {
+
+    public static final String RAPTOR = "raptor";
+    public static final String PATH_SEPARATOR = "/";
+    public static final String NODE_TYPE_SERVICE = "service";
+    public static final String NODE_TYPE_CLIENT = "client";
+    public static final String DEFAULT_CHARACTER = "utf-8";
+
+    public static final String HEADER_REQUEST_ID = "x-raptor-request-id";
+    public static final String HEADER_HOST_CLIENT = "x-raptor-host-client";
+    public static final String HEADER_HOST_SERVER = "x-raptor-host-server";
+    public static final String HEADER_TRACE_PREFIX = "x-raptortrace-";
+    public static final String HEADER_ERROR="x-raptor-error";
+
+}
